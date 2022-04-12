fa0f20dd1412a4eddbd7f503bfc805b735d53040
suniLPATIDARr <sunilpatidar1209@gmail.com>
2022-04-12 16:26:15 +0530

    closure def

--- closure/index.js
+++ closure/index.js
@@ -1,8 +1,10 @@
+// closure is created when a child function keep the environment
+//  of the parent scope even after the parent function has already executed 
 function outer() {
     let counter = 0;
     function inner(a) {
         counter += a;
-        console.log(counter)
+        console.log(counter);
     }
     return inner;
 }
